73da6d0caaa561e003b44e6c9f3064288044ad75
Kevin Lee <pvklee@ucla.edu>
2019-04-21 21:37:31 -0700

    created user model. created users and sessions controllers

---
+++ app/models/user.rb
@@ -0,0 +1,38 @@
+class User < ApplicationRecord
+    attr_reader :password
+    
+    validates :email, :session_token, presence: true, unique: true
+    validates :password_digest, presence: true
+
+    after_initialize :ensure_session_token
+
+    def password=(password)
+        @password = password
+        self.password_digest = BCrypt::Password.create(password)
+    end
+
+    def is_password?(password)
+        BCrypt::Password.new(self.password_digest).is_password?(password)
+    end
+
+    def User.find_by_credentials(email, password)
+        user = User.find_by(email: email)
+        return nil if user.nil?
+        user.is_password(password) ? user : nil
+    end
+
+    def self.generate_session_token!
+        SecureRandom::urlsafe_base64(16)
+    end
+
+    def reset_session_token!
+        self.session_token = self.class.generate_session_token
+        self.save!
+        self.session_token
+    end
+
+    private
+    def ensure_session_token
+        self.session_token ||= self.class.generate_session_token
+    end
+end
